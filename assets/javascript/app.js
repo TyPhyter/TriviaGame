6f816e5f35baa9bb9d3331ee80a69e41de91d5d6
TyPhyter <tylerjenkins08@gmail.com>
2018-02-28 18:17:55 -0600

    app scaffolding, initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+// * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+// * If the player selects the correct answer, show a screen congratulating them for choosing the right option.
+// After a few seconds, display the next question -- do this without user input.
+
+// * The scenario is similar for wrong answers and time-outs.
+
+//   * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+//   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. 
+//Wait a few seconds, then show the next question.
+
+// * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+var game = {
+
+    correctAnswers : 0,
+    incorrectAnswers : 0,
+
+    questions : [
+        {
+            question : "",
+            answers : [],
+            correctAnswerIndex : -1;
+        },
+    ],
+
+
+
+    newGame : { function(){
+        this.correctAnswers = 0;
+        this.incorrectAnswers = 0;
+    }},
+
+}
+
+game.newGame();
