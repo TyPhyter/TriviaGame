7539b965a0119b8e0a5134b2b42cc064b7e5aca7
TyPhyter <tylerjenkins08@gmail.com>
2018-03-06 02:09:46 -0600

    basic functionality prototyped

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,27 +10,112 @@
 //Wait a few seconds, then show the next question.
 
 // * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+var secondsPerQuestion = 10;
 
 var game = {
 
     correctAnswers : 0,
     incorrectAnswers : 0,
+    currentQuestionIndex: 0,
+    selectedAnswerIndex: -1,
+    timeRemaining: secondsPerQuestion,
+    questionTimer: null,
+    secondsTimer: null,
+
 
     questions : [
         {
-            question : "",
-            answers : [],
-            correctAnswerIndex : -1,
+            question : "Question Text",
+            answers : ["wrong", "right", "wrong", "wrong"],
+            correctAnswerIndex : 1,
+        },
+
+        {
+            question : "Another Question Text",
+            answers : ["wrong", "wrong", "wrong", "right"],
+            correctAnswerIndex : 3,
         },
     ],
 
+    updateDisplay : function() {
+        $("#play-area").html(`
+            <h1 class="title">TRIVIA TITLE</h1>
+            <div id="time-remaining">Time remaining: ${this.timeRemaining}</div>
+            <div id="question-text">${this.questions[this.currentQuestionIndex].question}</div>
+            <div class="list-container">
+                <ul id="answer-list">
+                    <li class="answer" data-index="0">${this.questions[this.currentQuestionIndex].answers[0]}</li>
+                    <li class="answer" data-index="1">${this.questions[this.currentQuestionIndex].answers[1]}</li>
+                    <li class="answer" data-index="2">${this.questions[this.currentQuestionIndex].answers[2]}</li>
+                    <li class="answer" data-index="3">${this.questions[this.currentQuestionIndex].answers[3]}</li>
+                </ul>
+            </div>
+        `);
+    },
 
-
-    newGame : { function(){
+    newGame : function() {
         this.correctAnswers = 0;
         this.incorrectAnswers = 0;
-    }},
+        this.currentQuestionIndex = 0;
+        this.selectedAnswerIndex = -1;
+
+        this.questionTimer = setTimeout(function() {
+            //
+            this.nextQuestion();
+        }.bind(this), 10000);
+        
+        this.secondsTimer = setInterval(function() {
+            if(this.timeRemaining > 0) {
+                this.timeRemaining--;
+                console.log(this.timeRemaining);
+                this.updateDisplay();
+            }
+        }.bind(this), 1000);
+
+        this.updateDisplay();
+        
+    },
+
+    nextQuestion : function() {
+
+        this.selectedAnswerIndex = -1;
+        this.currentQuestionIndex++;
+        this.timeRemaining = secondsPerQuestion;
+
+        clearTimeout(this.questionTimer);
+        clearInterval(this.secondsTimer);
+
+        this.questionTimer = setTimeout(function() {
+            //
+            this.nextQuestion();
+        }.bind(this), 10000);
+        
+        this.secondsTimer = setInterval(function() {
+            if(this.timeRemaining > 0) {
+                this.timeRemaining--;
+                console.log(this.timeRemaining);
+                this.updateDisplay();
+            }
+        }.bind(this), 1000);
+
+        this.updateDisplay();
+    }
 
 }
 
+$(document).on("click", ".answer", function() {
+    game.selectedAnswerIndex = $(this).attr("data-index");
+    if(parseInt(game.selectedAnswerIndex) === game.questions[game.currentQuestionIndex].correctAnswerIndex){
+        console.log("correct!");
+    } else {
+        console.log("wrong!");
+    } 
+    if(game.currentQuestionIndex < game.questions.length-1){
+        game.nextQuestion();
+    }
+});
+
 game.newGame();
+
+
+
